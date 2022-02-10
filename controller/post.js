d7c15992bd6fcf7c0c3d8dc93107a6ff14637507
MasonNguyen <mason_nguyen@mason.nguyen>
2022-02-10 13:54:01 +0700

    First

---
+++ controller/post.js
@@ -0,0 +1,21 @@
+const Post = require("../model/post");
+
+exports.getPosts = async (req, res) => {
+  const data = await Post.find();
+
+  res.status(200).json({
+    data: data,
+  });
+
+  res.end();
+};
+
+exports.createPost = (req, res) => {
+  const post = new Post(req.body);
+
+  post.save().then((result) => {
+    res.status(200).json({
+      post: result,
+    });
+  });
+};
